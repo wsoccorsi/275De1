c884275e0dbd1a0987aa857cb0182eb06edf4f54
wsoccorsi <wsoccorsi@gmail.com>
2019-10-28 12:20:09 -0400

    singlefragmentactivity change

--- app/src/main/java/com/example/salesapp/MainActivity.java
+++ app/src/main/java/com/example/salesapp/MainActivity.java
@@ -6,20 +6,12 @@ import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public abstract class MainActivity extends SingleFragmentActivity {
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
-
-        if (fragment == null) {
-            fragment = new ListingFragment();
-            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
-
-        }
+    protected Fragment createFragment() {
+        return new ListingFragment();
     }
+
 }
